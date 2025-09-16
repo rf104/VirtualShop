14c29efee1d2c68790e3a261fc973c00cc0f305e
rf104 <kingaref13@gmail.com>
2025-09-16 16:48:50 +0600

    Dynamically IP can be handel from now on just need to change the .env file!!

--- virtual_shop/lib/utils/product_service_rf.dart
+++ virtual_shop/lib/utils/product_service_rf.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import '../models/product_rf.dart'; // import your Product model
 
 class ProductService {
-  final String baseUrl = "http://192.168.0.154:8000"; // change to your backend
+  // final String baseUrl = "http://10.103.130.4:8000"; // change to your backend
+
+  final String baseUrl = "http://${dotenv.env['hostIp']}:${dotenv.env['PORT']}";
 
   Future<List<Product>> fetchProducts() async {
     final response = await http.get(Uri.parse('$baseUrl/products/'));
